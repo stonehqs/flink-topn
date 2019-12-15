dc06c6a261e6318e98250d3ec7f6af8ff9786ed2
stonehqs <68344150@qq.com>
2019-12-15 10:26:12 +0800

    remove target

--- target/classes/itembuycount.sql
+++
@@ -1,7 +0,0 @@
-CREATE TABLE `itembuycount` (
-  `id` mediumint NOT NULL auto_increment,
-  `itemId` bigint(255) NOT NULL,
-  `buyCount` bigint(11) DEFAULT NULL,
-  `createDate` timestamp NULL DEFAULT NULL,
-  PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci
